df5aa129cd50956e198de8d222349cb42e574694
TadPaneusz <pawel.aleksander.mnich@gmail.com>
2020-11-08 23:29:23 +0100

    Dodaj pliki projektów.

---
+++ Biblioteka/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace Biblioteka
+{
+    /// <summary>
+    /// Klasa służąca do tworzenia serwera TCP który przyjmuje liczby i zwraca ich pierwiastek
+    /// </summary>
+    public class ServerTCP
+    {
+        TcpListener listener;
+        TcpClient client;
+        NetworkStream stream;
+
+        IPAddress ip_address;
+        int port_number;
+
+        /// <summary>
+        /// Konstruktor pobierający adres IP oraz port
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        public ServerTCP(IPAddress ip, int port)
+        {
+            ip_address = ip;
+            port_number = port;
+
+            listener = new TcpListener(ip, port);
+        }
+        /// <summary>
+        /// Funkcja która kończy się dopiero gdy do serwera podepnie się klient
+        /// </summary>
+        public void WaitForClient()
+        {
+            listener.Start();
+            client = listener.AcceptTcpClient();
+            stream = client.GetStream();
+        }
+
+        /// <summary>
+        /// Funkcja która oczekuje na wiadomości oraz na nie odpowiada, działa aż do przesłania ciągu znaków EXIT
+        /// </summary>
+        public void ServerLoop()
+        {
+            byte[] buffer_get = new byte[1024];
+            byte[] buffer_send;
+
+            buffer_send = Encoding.UTF8.GetBytes("Wprowadzaj liczby pojedynczo. W odpowiedzi otrzymasz te liczby spierwiastkowane. Napisz EXIT jeśli chcesz wyjść. Zależnie od ustawień komputera jako przecinek stosuje się , lub .\r\n");
+            stream.Write(buffer_send, 0, buffer_send.Length);
+
+            while (true)
+            {
+                int size_recv = stream.Read(buffer_get, 0, 1024);
+
+                string str = Encoding.UTF8.GetString(buffer_get, 0, size_recv);
+
+                //Ignorowane są przejścia do następnej lini
+                if (str.Equals("\r\n")) { continue; }
+
+                //Zakończ pętle jeśli otrzymano string EXIT
+                if(str.Equals("EXIT")) { break; }
+
+                string output_string;
+                double input_number;
+
+                //Jeśli konwersja na typ double się uda wyślij wynik, jeśli nie to poinformuj użytkownika
+                if (Double.TryParse(str, out input_number))
+                {
+                    double result = Math.Sqrt(input_number);
+
+                    output_string = "sqrt(" + str + ") = " + result.ToString() + "\r\n";
+                }
+                else
+                {
+                    output_string = "Nie wprowadzono liczby.\r\n";
+                }
+
+                buffer_send = Encoding.UTF8.GetBytes(output_string);
+
+                stream.Write(buffer_send, 0, buffer_send.Length);
+            }
+        }
+    }
+}
---
+++ Laby1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Biblioteka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+
+namespace Laby1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerTCP server = new ServerTCP(IPAddress.Parse("127.0.0.1"), 2000);
+
+            server.WaitForClient(); //Serwer czeka na klienta
+
+            server.ServerLoop(); //Serwer czeka i odpowiada na wiadomości
+        }
+    }
+}
